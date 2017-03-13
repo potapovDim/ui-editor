048488930f3bb14be9371593368e93b13b3462ef
potapovDim <dmytrop@weblium.com>
2017-03-13 16:06:55 +0200

    fix

--- watir.test.rb
+++ watir.test.rb
@@ -3,16 +3,22 @@ require 'rubygems'
 require 'rspec'
 
 describe "Weblium" do
-  before :all do
-    @browser = Watir::Browser.new :chrome
-    @browser.goto 'localhost:8080'
+  browser = Watir::Browser.new :chrome
+  before :each do
+    browser.goto 'localhost:8080'
+    puts "before each !!!!"
   end
   it "click left nav " do
-    @browser.element(css: 'ul>li:nth-child(2)>button').click
-    @browser.element(css: '.previewElements__button_1jI').click
+    browser.element(css: 'ul>li:nth-child(2)>button').click
+    browser.element(css: '.previewElements__button_1jI').click
+  end
+  it "open block settings" do
+    browser.element(css: '[data-test="edit-button"]').fire_event "mousemove"
+    browser.element(css: '[data-test="edit-button"]').hover 
+    browser.element(css: '[data-test="edit-button"]').click
   end
   after :all do
-    @browser.quit
+    browser.quit
   end
 end
 
