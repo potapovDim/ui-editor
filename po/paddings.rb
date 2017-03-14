d69ea630585b6179e49e5c1a4706b7e8c04e8b97
potapovDim <dmytrop@weblium.com>
2017-03-14 10:30:37 +0200

    context panel

--- po/paddings.rb
+++ po/paddings.rb
@@ -1,10 +1,13 @@
 class PaddingsPO
+  #css selectors 
+  @@padding_top = '[data-test="block-padding-top"]'
+  @@padding_bottom = '[data-test="block-padding-bottom"]'
+  #browser instance
   @@browser
   def initialize(browser)
     @@browser = browser
   end
-  @@padding_top = '[data-test="block-padding-top"]'
-  @@padding_bottom = '[data-test="block-padding-bottom"]'
+  #methods
   def resize_padding_top_block(x, y)
     @@browser.element(css: @@padding_top).hover
     @@browser.wait(1500)
