2a146081c894bb105851673f8b21cc5a34c22cfd
potapovDim <dmytrop@weblium.com>
2017-03-27 15:41:00 +0300

    refactor;

---
+++ po/left-nav/pages/create-new-page.rb
@@ -0,0 +1,29 @@
+module CreateNewPage
+  #left nav paget 
+  #css selectors
+  @page_name                     = '#CreatePageInput1'
+  @page_url                      = '#CreatePageInput2'
+  @cancel_button                 = 'button[title="Cancel"]'
+  @add_page_button               = 'button[title="Add page"]'
+  #initialize browser
+  @browser
+  def self.initBrowser(browser)
+    @browser = browser
+    return self
+  end
+  #api methods
+  def self.enter_page_name(name)
+    @browser.element(css: @page_name).send_keys name
+    return self
+  end
+  def self.enter_page_url(url)
+    @browser.element(css: @page_url).send_keys name
+    return self
+  end
+  def self.cancel_add_page
+    @browser.element(css: @cancel_button).click
+  end
+  def self.add_page
+    @browser.element(css: @add_page_button).click
+  end
+end
