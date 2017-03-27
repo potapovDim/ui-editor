177d128c15a1842defe15dc154aa7c4d8c7a8422
potapovDim <dmytrop@weblium.com>
2017-03-27 15:01:59 +0300

    add row

---
+++ po/row/index.rb
@@ -0,0 +1,28 @@
+require_relative "paddings"
+require_relative "context-panel"
+
+class Row
+  include ContextPanel
+  #initialize drivers
+  @@browser
+  def initialize(browser)
+    @@browser = browser
+    ContextPanel.initBrowser browser
+  end
+  #paddings api
+  def resize_padding()
+    return PaddingsBlock.initBrowser @@browser
+  end
+  #block context panel api
+  def click_row_context (button, index = 0)
+    case button
+      when "remove"
+        ContextPanel.row_remove(index)
+      when "duplicate"
+        ContextPanel.row_duplicate(index)
+      when "settings"
+        ContextPanel.open_row_settings(index)
+        return Settings.initBrowser @@browser
+    end
+  end
+end
---
+++ po/row/settings/index.rb
@@ -0,0 +1,28 @@
+require_relative "../../share/settings-background-tab"
+require_relative "./layout"
+
+module Settings
+  include BackgroundTab
+  include LayoutTab
+  #css selectors
+  @background_tab = 'Background'
+  @layout_tab = 'Layout'
+
+  #browser instance
+  @browser
+  def self.initBrowser(browser)
+    @browser = browser
+    return self
+  end
+  #background tab api
+  def self.choose_tab(tab = "background")
+    case tab
+      when "background"
+        @browser.element(text: @background_tab).click
+        return BackgroundTab.initBrowser @browser
+      when "layout"
+        @browser.element(text: @layout_tab).click
+        return LayoutTab.initBrowser @browser
+    end
+  end
+end
