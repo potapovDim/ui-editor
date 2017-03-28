7031d4e14b0f685e454e07b3380f0d6f64f64325
potapovDim <dmytrop@weblium.com>
2017-03-28 16:33:08 +0300

    add po

---
+++ po/left-nav/pages/index.rb
@@ -0,0 +1,24 @@
+module Pages
+  #pages
+  #css selectors
+  @page                          = '[data-test="item-node-target"]'
+  @page_buttons                  = '[data-test="layout-button"]'
+  #initialize browser
+  @browser
+  def self.initBrowser(browser)
+    @browser = browser
+    return self
+  end
+  #api methods
+  def self.open_page_settings
+    @browser.elements(css: @page_buttons)[0].click
+    return self
+  end
+  def self.dublicate_page
+    @browser.elements(css: @page_buttons)[1].click
+    return self
+  end
+  def self.delete_page
+    @browser.elements(css: @page_buttons)[2].click
+  end
+end
